1a5b30ebada7a9616333d2ad848456af69e1f209
MuthyalaChandana5 <120270006+MuthyalaChandana5@users.noreply.github.com>
2022-12-28 23:34:54 +0530

    acd

--- Assignments/src/com/innerclass/Outer.java
+++ Assignments/src/com/innerclass/Outer.java
@@ -14,11 +14,11 @@ public class Outer {
 			   LocalInner i = new LocalInner();
 			   i.m2();
 		}
-		
-	public static void main(String args[]) {
+	
+public static void main(String args[]) {
 		 Outer o = new Outer();
 		 o.m1();
 		
 
-	}
+}
 }
